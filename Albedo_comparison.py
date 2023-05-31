cd7d5ed715f16334039d2ec0f6263b0f65f08492
tijmen98 <67052913+tijmen98@users.noreply.github.com>
2023-05-31 17:11:41 +0200

    new iteration

--- Albedo_comparison.py
+++ Albedo_comparison.py
@@ -18,6 +18,11 @@ snowfall_event = [3, 8]
 first_alb = float(0.75)
 
 
+def surface_albedo_to_clear_sky(Albedo):
+    # Chen et al 1983
+    return(0.0587+0.730*Albedo)
+
+
 def THEORY_ALB_NORMAL(MIN_ALB, MAX_ALB, start_alb, RELAX, timerange):
     albedo = np.zeros(len(timerange))
     albedo[0] = start_alb
@@ -57,15 +62,33 @@ WEATHER = pd.read_csv('/Volumes/Tijmen/Master-Thesis/Data/Aws_data/SODANKYLA_WEA
           year + '-01-01':year + '-12-31']
 RADIATION = pd.read_csv('/Volumes/Tijmen/Master-Thesis/Data/Aws_data/' + year + '/SODANKYLA_RADIATION_daily.csv',
                         index_col=0)
-MODIS = xr.open_dataset('/Volumes/Tijmen/Master-Thesis/Data/MODIS/'+year+'_RCG.nc').sel(rlat=26.5, rlon=-11, method='nearest').to_pandas()
+MODIS = xr.open_dataset('/Volumes/Tijmen/Master-Thesis/Data/MODIS/'+year+'_RCG.nc').sel(rlat=26.5,
+                                                                                        rlon=-11,
+                                                                                        method='nearest').to_pandas()
+
+RACMO_ALBEDO = xr.open_dataset('/Volumes/Tijmen/Master-Thesis/Data/RACMO_2.4/PXARC11/2001_new/NC_MD/Clearsky_albedo_calculated.nc')['Clear-sky_albedo'].sel(rlat=26.5,
+                                                                                        rlon=-11,
+                                                                                        method='nearest').to_pandas().loc[
+          year + '-01-01':year + '-12-31']
+
+RACMO_SNOWFALL = xr.open_dataset('/Volumes/Tijmen/Master-Thesis/Data/RACMO_2.4/PXARC11/2001_new/NC_DEFAULT/pr.KNMI-2001.PXARC11.RACMO24_1_complete6_UAR_q_noice_khalo6_era5q.DD.nc')['pr'].sel(rlat=26.5,
+                                                                                        rlon=-11,
+                                                                                        method='nearest').to_pandas().loc[
+          year + '-01-01':year + '-12-31']
+
+RACMO_SNOWDEPTH = xr.open_dataset('/Volumes/Tijmen/Master-Thesis/Data/RACMO_2.4/PXARC11/2001_new/NC_DEFAULT/sndp.KNMI-2001.PXARC11.RACMO24_1_complete6_UAR_q_noice_khalo6_era5q.DD.nc')['sndp'].sel(rlat=26.5,
+                                                                                        rlon=-11,
+                                                                                        method='nearest').to_pandas().loc[
+          year + '-01-01':year + '-12-31']
 
 
-"""AWS FIND MOMENTS WHERE SNOW ACCUMULATES AND EXTRACT ALBEDO TIMESERIES AROUND THESE MOMENTS"""
+"""AWS AND MODIS FIND MOMENTS WHERE SNOW ACCUMULATES AND EXTRACT ALBEDO TIMESERIES AROUND THESE MOMENTS"""
 
 Precep = WEATHER[WEATHER['Precipitation amount (mm)'] > 2.]
 Snowfall = Precep[Precep['Maximum temperature (degC)'] < 0.]
 aws_events = []
 model_events = []
+modis_events = []
 for event in range(len(Snowfall.index)):
 
     start_date = pd.to_datetime(Snowfall.index[event]) - timedelta(days=5)
@@ -77,37 +100,41 @@ for event in range(len(Snowfall.index)):
         continue
 
     snowfall = WEATHER.loc[str(start_date):str(end_date)]['Precipitation amount (mm)']
-
     snowfall_events = []
 
     for i in range(len(snowfall)):
         if snowfall.iloc[i] > 2:
             snowfall_events.append(i)
-    model_events.append(THEORY_ALB_SNOWFALL(MIN_ALB, MAX_ALB, first_alb, COLD_SNOW_RELAX, timerange, snowfall_events))
-    axs[1].plot(timerange, THEORY_ALB_SNOWFALL(MIN_ALB, MAX_ALB, first_alb, COLD_SNOW_RELAX, timerange, snowfall_events), color='red', linewidth=0.5)
+
 
     AWS_ALBEDO = abs(RADIATION.loc[str(start_date):str(end_date)]['Reflected radiation (W/m2)']) / abs(
         RADIATION.loc[str(start_date):str(end_date)]['Global radiation (W/m2)'])
     AWS_SNOWDEPTH = WEATHER.loc[str(start_date):str(end_date)]['Snow depth (cm)']
-    aws_events.append(AWS_ALBEDO.values)
-    axs[1].plot(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values, linewidth=0.5, color='orange')
-    axs[1].plot(AWS_ALBEDO.values, linewidth=0.5, color='green')
 
+    aws_events.append(AWS_ALBEDO.values)
+    model_events.append(THEORY_ALB_SNOWFALL(MIN_ALB, MAX_ALB, first_alb, COLD_SNOW_RELAX, timerange, snowfall_events))
+    modis_events.append(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values)
+
+    axs[1].plot(timerange, surface_albedo_to_clear_sky(THEORY_ALB_SNOWFALL(MIN_ALB, MAX_ALB, first_alb, COLD_SNOW_RELAX, timerange, snowfall_events)), color='red', linewidth=0.5)
+    axs[1].plot(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values, linewidth=0.5, color='orange')
+    axs[1].plot(surface_albedo_to_clear_sky(AWS_ALBEDO.values), linewidth=0.5, color='green')
+
+mean_modis_event = np.mean(modis_events, axis=0)
 mean_event = np.mean(aws_events, axis=0)
 mean_model_event = np.mean(model_events, axis=0)
 axs[0].plot(mean_model_event, linewidth=1.5, color='blue', linestyle=(0, (1, 1)))
 axs[0].plot(mean_event, linewidth=1.5, color='blue')
+axs[0].plot(mean_modis_event, linewidth=1.5, color='blue', linestyle=(0, (2, 2)))
 
-"""AWS FIND MOMENTS WHERE SNOW MELTS AND EXTRACT ALBEDO TIMESERIES AROUND THESE MOMENTS"""
+"""AWS AND MODIS FIND MOMENTS WHERE SNOW MELTS AND EXTRACT ALBEDO TIMESERIES AROUND THESE MOMENTS"""
 
 warm = WEATHER[WEATHER['Maximum temperature (degC)'] > 0.]
 aws_events = []
 model_events = []
+modis_events = []
 for event in range(len(warm.index)):
     start_date = pd.to_datetime(warm.index[event]) - timedelta(days=5)
     end_date = pd.to_datetime(warm.index[event]) + timedelta(days=10)
-    print(WEATHER.loc[str(pd.to_datetime(warm.index[event])).split(' ')[0], 'Snow depth (cm)'])
-    print(WEATHER.loc[str(pd.to_datetime(warm.index[event])+timedelta(days=1)).split(' ')[0], 'Snow depth (cm)'])
 
     if WEATHER.loc[str(pd.to_datetime(warm.index[event])).split(' ')[0], 'Snow depth (cm)'] -2 < WEATHER.loc[str(pd.to_datetime(warm.index[event])+timedelta(days=1)).split(' ')[0], 'Snow depth (cm)']:
         continue
@@ -122,20 +149,107 @@ for event in range(len(warm.index)):
         if snowfall.iloc[i] > 2:
             snowfall_events.append(i)
 
-    model_events.append(THEORY_ALB_MELT(MIN_ALB, MAX_ALB, first_alb, MELT_RELAX, timerange, snowfall_events))
-    axs[2].plot(timerange, THEORY_ALB_MELT(MIN_ALB, MAX_ALB, first_alb, MELT_RELAX, timerange, snowfall_events), color='red', linewidth=0.5)
+
 
     AWS_ALBEDO = abs(RADIATION.loc[str(start_date):str(end_date)]['Reflected radiation (W/m2)']) / abs(
         RADIATION.loc[str(start_date):str(end_date)]['Global radiation (W/m2)'])
     AWS_SNOWDEPTH = WEATHER.loc[str(start_date):str(end_date)]['Snow depth (cm)']
-    aws_events.append(AWS_ALBEDO.values)
 
+    aws_events.append(AWS_ALBEDO.values)
+    modis_events.append(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values)
+    model_events.append(THEORY_ALB_MELT(MIN_ALB, MAX_ALB, first_alb, MELT_RELAX, timerange, snowfall_events))
+
+    axs[2].plot(timerange, surface_albedo_to_clear_sky(THEORY_ALB_MELT(MIN_ALB, MAX_ALB, first_alb, MELT_RELAX, timerange, snowfall_events)), color='red', linewidth=0.5)
     axs[2].plot(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values, linewidth=0.5, color='orange')
-    axs[2].plot(AWS_ALBEDO.values, linewidth=0.5, color='green')
+    axs[2].plot(surface_albedo_to_clear_sky(AWS_ALBEDO.values), linewidth=0.5, color='green')
+
+mean_modis_event = np.mean(modis_events, axis=0)
 mean_event = np.mean(aws_events, axis=0)
 mean_model_event = np.mean(model_events, axis=0)
 axs[0].plot(mean_model_event, linewidth=1.5, color='red', linestyle=(0, (1, 1)))
 axs[0].plot(mean_event, linewidth=1.5, color='red')
+axs[0].plot(mean_modis_event, linewidth=1.5, color='red', linestyle=(0, (2, 2)))
+
+"""RACMO FIND MOMENTS WHERE SNOW ACCUMULATES AND EXTRACT ALBEDO TIMESERIES AROUND THESE MOMENTS"""
+
+Precep = RACMO_SNOWFALL[RACMO_SNOWDEPTH > 2.]
+racmo_events = []
+model_events = []
+modis_events = []
+for event in range(len(Precep.index)):
+
+    start_date = pd.to_datetime(Snowfall.index[event]) - timedelta(days=5)
+    end_date = pd.to_datetime(Snowfall.index[event]) + timedelta(days=10)
+
+    if RACMO_SNOWDEPTH[0].loc[str(start_date):str(end_date)].mean() < 10:
+        continue
+
+    snowfall = RACMO_SNOWFALL[0].loc[str(start_date):str(end_date)]
+    snowfall_events = []
+
+    for i in range(len(snowfall)):
+        if snowfall.iloc[i] > 2:
+            snowfall_events.append(i)
+
+    racmo_events.append(RACMO_ALBEDO.loc[str(start_date):str(end_date)].values)
+    model_events.append(THEORY_ALB_SNOWFALL(MIN_ALB, MAX_ALB, first_alb, COLD_SNOW_RELAX, timerange, snowfall_events))
+    modis_events.append(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values)
+
+    axs[1].plot(timerange, surface_albedo_to_clear_sky(THEORY_ALB_SNOWFALL(MIN_ALB, MAX_ALB, first_alb, COLD_SNOW_RELAX, timerange, snowfall_events)), color='red', linewidth=0.5)
+    axs[1].plot(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values, linewidth=0.5, color='orange')
+    axs[1].plot(surface_albedo_to_clear_sky(RACMO_ALBEDO.loc[str(start_date):str(end_date)].values), linewidth=0.5, color='green')
+
+mean_modis_event = np.mean(modis_events, axis=0)
+mean_racmo_event = np.mean(racmo_events, axis=0)
+mean_model_event = np.mean(model_events, axis=0)
+axs[0].plot(mean_model_event, linewidth=1.5, color='blue', linestyle=(0, (1, 1)))
+axs[0].plot(mean_racmo_event, linewidth=1.5, color='blue')
+axs[0].plot(mean_modis_event, linewidth=1.5, color='blue', linestyle=(0, (2, 2)))
+
+"""RACMO FIND MOMENTS WHERE SNOW MELTS AND EXTRACT ALBEDO TIMESERIES AROUND THESE MOMENTS"""
+
+warm = WEATHER[WEATHER['Maximum temperature (degC)'] > 0.]
+aws_events = []
+model_events = []
+modis_events = []
+for event in range(len(warm.index)):
+    start_date = pd.to_datetime(warm.index[event]) - timedelta(days=5)
+    end_date = pd.to_datetime(warm.index[event]) + timedelta(days=10)
+
+    if WEATHER.loc[str(pd.to_datetime(warm.index[event])).split(' ')[0], 'Snow depth (cm)'] -2 < WEATHER.loc[str(pd.to_datetime(warm.index[event])+timedelta(days=1)).split(' ')[0], 'Snow depth (cm)']:
+        continue
+    if WEATHER.loc[str(start_date):str(end_date)]['Snow depth (cm)'].mean() < 10:
+        continue
+
+    snowfall = WEATHER.loc[str(start_date):str(end_date)]['Precipitation amount (mm)']
+
+    snowfall_events = []
+
+    for i in range(len(snowfall)):
+        if snowfall.iloc[i] > 2:
+            snowfall_events.append(i)
+
+
+
+    AWS_ALBEDO = abs(RADIATION.loc[str(start_date):str(end_date)]['Reflected radiation (W/m2)']) / abs(
+        RADIATION.loc[str(start_date):str(end_date)]['Global radiation (W/m2)'])
+    AWS_SNOWDEPTH = WEATHER.loc[str(start_date):str(end_date)]['Snow depth (cm)']
+
+    aws_events.append(AWS_ALBEDO.values)
+    modis_events.append(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values)
+    model_events.append(THEORY_ALB_MELT(MIN_ALB, MAX_ALB, first_alb, MELT_RELAX, timerange, snowfall_events))
+
+    axs[2].plot(timerange, surface_albedo_to_clear_sky(THEORY_ALB_MELT(MIN_ALB, MAX_ALB, first_alb, MELT_RELAX, timerange, snowfall_events)), color='red', linewidth=0.5)
+    axs[2].plot(MODIS.loc[str(start_date):str(end_date)]['Albedo'].values, linewidth=0.5, color='orange')
+    axs[2].plot(surface_albedo_to_clear_sky(AWS_ALBEDO.values), linewidth=0.5, color='green')
+
+mean_modis_event = np.mean(modis_events, axis=0)
+mean_event = np.mean(aws_events, axis=0)
+mean_model_event = np.mean(model_events, axis=0)
+axs[0].plot(mean_model_event, linewidth=1.5, color='red', linestyle=(0, (1, 1)))
+axs[0].plot(mean_event, linewidth=1.5, color='red')
+axs[0].plot(mean_modis_event, linewidth=1.5, color='red', linestyle=(0, (2, 2)))
+
 
 axs[0].set_ylim(0,1)
 axs[1].set_ylim(0,1)
