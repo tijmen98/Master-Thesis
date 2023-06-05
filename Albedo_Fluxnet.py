70fe0c527971b537386a3241276d50dfca31fd1b
tijmen98 <67052913+tijmen98@users.noreply.github.com>
2023-06-05 18:17:15 +0200

    new iteration

--- Albedo_Fluxnet.py
+++ Albedo_Fluxnet.py
@@ -1,50 +1,71 @@
-"""Theoratical"""
-
-MIN_ALB = 0.5
-MAX_ALB = 0.85
-
-COLD_SNOW_RELAX = 0.008
-MELT_RELAX = 0.24
-
-timerange = np.arange(0,15)
-snowfall_event = [3, 8]
-first_alb = float(0.75)
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
 
-def surface_albedo_to_clear_sky(Albedo):
+def surface_albedo_to_clear_sky(albedo):
     # Chen et al 1983
-    return(0.0587+0.730*Albedo)
-
-def Albedo_Scheme(MIN_ALB, MAX_ALB, COLD_RELAX, WARM_RELAX, SNOWFALL, TEMPERATURE):
+    return 0.0587+0.730*albedo
 
 
-
-def THEORY_ALB_NORMAL(MIN_ALB, MAX_ALB, start_alb, RELAX, timerange):
+def albedo_Scheme(snowfall, temperature, start_alb=float(0.75), cold_relax=0.008, warm_relax=0.85, min_alb=0.5,
+                  max_alb=0.85):
+    timerange = snowfall.index
     albedo = np.zeros(len(timerange))
     albedo[0] = start_alb
     for time in timerange[0:-1]:
-        albedo[time+1] = albedo[time] - RELAX
-        if albedo[time+1] < MIN_ALB:
-            albedo[time+1] = MIN_ALB
-    return(albedo)
-def THEORY_ALB_SNOWFALL(MIN_ALB, MAX_ALB, start_alb, RELAX, timerange, snowfall_time):
-    albedo = np.zeros(len(timerange))
-    albedo[0] = start_alb
-    for time in timerange[0:-1]:
-        albedo[time+1] = albedo[time] - RELAX
-        if albedo[time+1] < MIN_ALB:
-            albedo[time+1] = MIN_ALB
+        if temperature[time+1] > 0:
+            albedo[time+1] = albedo[time] - warm_relax
+        else:
+            albedo[time + 1] = (albedo[time] - min_alb) * np.exp(-cold_relax) + min_alb
 
-        if time in snowfall_time:
-            albedo[time+1] = MAX_ALB
-    return(albedo)
-def THEORY_ALB_MELT(MIN_ALB, MAX_ALB, start_alb, RELAX, time, snowfall_time):
-    albedo = np.zeros(len(timerange))
-    albedo[0] = start_alb
-    for time in timerange[0:-1]:
-        albedo[time+1] = (albedo[time] - MIN_ALB) * np.exp(-RELAX) +MIN_ALB
-        if albedo[time+1] < MIN_ALB:
-            albedo[time+1] = MIN_ALB
-        if time in snowfall_time:
-            albedo[time+1] = MAX_ALB
-    return(albedo)
+        albedo[time+1] = albedo[time+1] + (np.min(np.max(snowfall[time+1], 0)/10, 1) * (max_alb - albedo[time+1]))
+
+        if albedo[time+1] < min_alb:
+            albedo[time+1] = min_alb
+
+    return albedo
+
+"""Define directories"""
+fluxnet_main_dir = '/Volumes/Tijmen/Master-Thesis/Data/Fluxnet/'
+fluxnet_data_dir = '/Volumes/Tijmen/Master-Thesis/Data/Fluxnet/Daily_fullset_Fluxnet/'
+
+"""Open in_situ/fluxnet dir"""
+ds_fluxnet_in_situ = pd.read_csv(fluxnet_main_dir+'fluxnet_in_situ.csv')
+
+"""Import data from fluxnet"""
+files = os.listdir(fluxnet_data_dir)
+for file in files:
+    if file[0] != 'F':
+        files.remove(file)
+
+new_files = files.copy()
+print(len(new_files))
+
+for file in files:
+    file_test = file.split('_')[1]
+    cont = False
+    for loc in ds_fluxnet_in_situ.loc[:, 'Fluxnet'].values:
+        if file.split('_')[1] == loc:
+            cont = True
+
+    if not cont:
+        continue
+
+    ds_fluxnet = pd.read_csv(fluxnet_data_dir+file, index_col=0)
+
+    """Functions"""
+
+    """Extract data from fluxnet dataset"""
+    try:
+        ds_sel = ds_fluxnet.loc[:, ['SW_IN_MDS', 'SW_OUT', 'P_F', 'TA_F_MDS']]
+    except:
+        new_files.remove(file)
+        continue
+
+    ds_sel[ds_sel == -9999] = np.nan
+    ds_sel.loc['Albedo']=ds_sel['SW_OUT']/ds_sel['SW_IN_MDS']
+
+    print(1)
