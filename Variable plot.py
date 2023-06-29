c1ca432a657a1c260e4430889c57793d487f0936
tijmen98 <67052913+tijmen98@users.noreply.github.com>
2023-06-29 11:50:37 +0200

    new iteration

--- Variable plot.py
+++ Variable plot.py
@@ -27,7 +27,7 @@ import Thesis_Functions.plotting as plotting
 datadir = "/Volumes/Tijmen/Master-Thesis/Data/"
 
 year = str(2002)
-time = 15
+time = 100
 safevariable = 'ALBEDO'
 variable = 'Clear-sky_albedo'
 
@@ -39,9 +39,11 @@ if desktop:
     datadir = "E:/Master-Thesis/Data/"
 
 racmo_ds = xr.open_dataset(datadir+'RACMO_2.4/PXARC11/2001_new/NC_MD/Clearsky_albedo_calculated.nc')
-modis_ds = xr.open_dataset(datadir+'MODIS/'+year+'_RCG.nc')
+modis_ds = xr.open_dataset(datadir+'RACMO_2.4/PXARC11/2001_v2/NC_MD/Clearsky_albedo_calculated.nc')
 racmo_albedo = racmo_ds[variable].sel(time=slice(year+'/01/01',year+'/12/31')).isel(time=time).squeeze()
-modis_albedo = modis_ds['Albedo'].isel(time=time)
+modis_albedo = modis_ds[variable].sel(time=slice(year+'/01/01',year+'/12/31')).isel(time=time).squeeze()
+
+racmo_albedo = racmo_albedo-modis_albedo
 
 rlon = racmo_ds.rlon.values
 rlat = racmo_ds.rlat.values
@@ -51,7 +53,7 @@ plt.figure(figsize=(15, 5), dpi=400)
 data_crs = ccrs.RotatedPole(pole_longitude=racmo_ds.rotated_pole.grid_north_pole_longitude,
                             pole_latitude=racmo_ds.rotated_pole.grid_north_pole_latitude)
 levels=20
-norm = colors.Normalize(vmin=0, vmax=1)
+norm = colors.Normalize(vmin=-0.2, vmax=0.2)
 
 ax1 = plt.subplot(1, 2, 1, projection=ccrs.Stereographic(central_longitude=0., central_latitude=90.) )
 result1 = ax1.contourf(rlon, rlat, racmo_albedo, levels=levels, norm=norm, extend='both', cmap='coolwarm', transform=data_crs)
@@ -65,8 +67,8 @@ ax2.coastlines(resolution='50m')
 plt.colorbar(result2, orientation='horizontal', label='Albedo', extend='both', fraction=0.046, pad=0.04)
 ax2.set_title('Modis clear sky albedo '+year, size='xx-large')
 
-plt.savefig('/Users/tijmen/Desktop/Figures_Thesis/'+safevariable+'test.jpeg', dpi=400)
-plt.close()
+plt.savefig('/Users/tijmen/Desktop/Figures_Thesis/Modis_V1-V2.jpeg', dpi=400)
 
+plt.close()
 
 print('')
